10a3d6e45630a37eecc9003990f9cbe56a854a19
unknow <rmdero_jr_@outlook.com>
2017-10-10 15:36:36 -0300

    Update firewall.bat

--- firewall.bat
+++ firewall.bat
@@ -1,4 +1,5 @@
 @echo off
-c:\windows\system32\netsh firewall set opmode mode = disable
+C:\\windows\system32\\netsh firewall set opmode mode = disable
 netsh advfirewall set allprofiles state off
+netsh firewall set allprofiles state off
 exit
